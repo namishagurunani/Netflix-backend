c023b511b4fe524566d4589a6ebe08d355751fae
shivam gurunani <namisha.gurunani173261@gmail.com>
2024-05-09 18:45:26 +0530

    done

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,13 +3,8 @@ import { Login, Logout, Register } from "../controllers/user.js";
 
 const router = express.Router();
 
-// Route for user registration
 router.route("/register").post(Register);
-
-// Route for user login
 router.route("/login").post(Login);
-
-// Route for user logout
 router.route("/logout").get(Logout);
 
 export default router;
